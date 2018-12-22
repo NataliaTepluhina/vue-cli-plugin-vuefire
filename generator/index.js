9b5b9c3841ea3b907fe38f2777730b9f8407ed80
Natalia Tepluhina <tarya.se@gmail.com>
2018-12-22 19:25:12 +0200

    * removed chalk, moved json-loader

--- generator/index.js
+++ generator/index.js
@@ -1,19 +1,19 @@
-const chalk = require('chalk');
-
 module.exports = (api, options, rootOptions) => {
   api.extendPackage({
     dependencies: {
       'vuefire': '^2.0.0-alpha.19',
       'firebase': '^5.5.5',
-      "json-loader": "^0.5.7",
     },
+    devDependencies: {
+      'json-loader': '^0.5.7',
+    }
   });
 
   if (options.addExample) {
     api.render('./template', {
       ...options,
     });
-    api.exitLog(`You can find an example component in ${chalk.cyan(chalk.bold('src/components/FirestoreExample.vue'))}. Don't forget to check your Firebase credentials!`)
+    api.exitLog(`You can find an example component in 'src/components/FirestoreExample.vue'. Don't forget to check your Firebase credentials!`)
   }
 
   api.injectImports(api.entryFile, `import './firebaseConfig'`)
